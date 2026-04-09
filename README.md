3d851dcecdb9cb4ed2d14afa3a4a789a0e227a2e
Padma Priya K V <padmapriyakv01@gmail.com>
2026-04-09 21:30:16 +0530

    Add files via upload

--- README.md
+++
@@ -1,1 +0,0 @@
-# dockerize-simple-webapp
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+<<<<<<< HEAD
 import os
+=======
+>>>>>>> 2ad3f05 (Add files via upload)
 
 # Create Flask app
 app = Flask(__name__)
@@ -11,6 +14,11 @@ def home():
 
 # Run the app
 if __name__ == '__main__':
+<<<<<<< HEAD
     # Get port from environment (for Render)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+=======
+    # host=0.0.0.0 allows access outside Docker container
+    app.run(host='0.0.0.0', port=5000)
+>>>>>>> 2ad3f05 (Add files via upload)
