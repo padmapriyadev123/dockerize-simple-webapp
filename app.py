b039b48575924536d5df2404cf94e07c12520b09
Padma Priya K V <padmapriyakv01@gmail.com>
2026-04-09 22:03:59 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 
 # Create Flask app
@@ -11,4 +12,19 @@ def home():
 # Run the app
 if __name__ == '__main__':
     # host=0.0.0.0 allows access outside Docker container
+=======
+from flask import Flask
+
+# Create Flask app
+app = Flask(__name__)
+
+# Homepage route
+@app.route('/')
+def home():
+    return "Hello! My DevOps Project is Running 🚀"
+
+# Run the app
+if __name__ == '__main__':
+    # host=0.0.0.0 allows access outside Docker container
+>>>>>>> 2ad3f051ffbff9141e4c8e475c415afe40ec02d4
     app.run(host='0.0.0.0', port=5000)
