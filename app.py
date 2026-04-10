0a46313d2e2ec7e4968e54e00ec2917374f3a04d
Padma Priya K V <padmapriyakv01@gmail.com>
2026-04-10 16:14:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 from flask import Flask
+import os
 
 # Create Flask app
 app = Flask(__name__)
@@ -11,20 +11,6 @@ def home():
 
 # Run the app
 if __name__ == '__main__':
-    # host=0.0.0.0 allows access outside Docker container
-=======
-from flask import Flask
-
-# Create Flask app
-app = Flask(__name__)
-
-# Homepage route
-@app.route('/')
-def home():
-    return "Hello! My DevOps Project is Running 🚀"
-
-# Run the app
-if __name__ == '__main__':
-    # host=0.0.0.0 allows access outside Docker container
->>>>>>> 2ad3f051ffbff9141e4c8e475c415afe40ec02d4
-    app.run(host='0.0.0.0', port=5000)
+    # Get port from environment (for Render)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
